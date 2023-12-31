d406867492e6dd73d437c7290aeeeafb25f9712d
Baqar Ali <sayedalintl@gmail.com>
2024-01-01 00:22:59 +0530

    complete todo

--- script.js
+++ script.js
@@ -1,70 +1,88 @@
 const input = document.getElementById("input");
 const btn = document.querySelector(".btn");
 const showTodo = document.querySelector(".showTodo");
-let json = [];
+
 let index = 0;
 let str = " ";
 
-
-
-
-
-let todos = JSON.parse(localStorage.getItem("todos"));
-if(todos){
-     index = -1
-    todos.forEach(element =>{
-      index++;
-      json.push({id:index,text:element.text});
-      localStorage.setItem("todos",JSON.stringify(json))
-      str += `<p>${element.text}<span onclick="deleteTodo(${element.id})" class="delete">Delete</span><span class="Edit" onclick="EditTodo(${element.id})">Edit</span></p>`
-    });
-    showTodo.innerHTML = str;
-  }
-
-  // add Event Listener click
-btn.addEventListener("click", () => {
- if(btn.innerHTML === "Add"){
-  str += `<p>${input.value}<span onclick="deleteTodo(${index})" class="delete">Delete</span><span onclick="EditTodo(${index})" class="Edit">Edit</span></p>`
-  showTodo.innerHTML = str;
-  setLocalValues();
-  input.value = " ";
- }  
-});
-function setLocalValues(){
-  todos = localStorage.getItem("todos");
-  if(todos == null){
-    index = 0;
-    json.push({id:index,text:input.value});
-    localStorage.setItem("todos",JSON.stringify(json))
-  }
-  else{
-    index++;
-    json.push({id:index,text:input.value});
-    localStorage.setItem("todos",JSON.stringify(json));
-    
-  }
+const onDelete = (i) =>{
+  let todos = JSON.parse(localStorage.getItem("todos"));
+  todos = todos.filter(todo=>todo.id !== i);
+  localStorage.setItem("todos",JSON.stringify(todos));
+  showTodos()
 }
 
-function deleteTodo(i) {
-  json.splice(i,1);
-  localStorage.setItem("todos",JSON.stringify(json))
-}
-function EditTodo(i){
-  let EditInputValue = json.findIndex(element=>element.id === i);
-  // console.log(i,EditInputValue);
-   input.value = json[EditInputValue].text;
-   
-   let findData = json.find((element,index)=>element.text === input.value);
+const onEdit = (i) =>{
+  let todos = JSON.parse(localStorage.getItem("todos"));
+  console.log(i);
+  
+  // show edit data in input
+  
+  let find_Data_forEdit = todos.findIndex(todo=>todo.id === i);
+  input.value = todos[find_Data_forEdit].text;
+  
+  // find data from input
+  let findInputData = todos.find(todo=>todo.text === input.value);
+  console.log(findInputData);
+  
   btn.innerHTML = "Edit";
   btn.addEventListener('click',()=>{
    if(btn.innerHTML === "Edit"){
-     let replacement = {...findData,text:input.value};
-     json.splice(i,1,replacement);
-     localStorage.setItem("todos",JSON.stringify(json))
+     todos = JSON.parse(localStorage.getItem("todos"));
+     let replacedata = {...findInputData,text:input.value};
+     console.log(replacedata);
+     todos.splice(find_Data_forEdit,1,replacedata);
+     localStorage.setItem("todos",JSON.stringify(todos));
+     showTodos()
+     input.value = "";
    }
   })
+
+  
   
 }
+
+const showTodos = () =>{
+  showTodo.innerHTML = "";
+let todos = JSON.parse(localStorage.getItem("todos"));
+if(todos){
+  todos.forEach(todo=>{
+    str = `<p>${todo.text} <i onclick="onDelete(${todo.id})" class=" delete fa-solid fa-delete-left"></i> <i onclick="onEdit(${todo.id})" class="Edit fa-solid fa-file-pen"></i></p>`
+    showTodo.innerHTML += str
+  })
+} 
+else{
+  showTodo.innerHTML = "No Todos Here";
+}
+}
+showTodos()
+
+
+
+btn.addEventListener('click',()=>{
+  if(btn.innerHTML == "Add"){
+    let todos = localStorage.getItem("todos");
+    if(todos == null){
+     let json = [];
+     index = 0;
+     json.push({id:index,text:input.value});
+     localStorage.setItem("todos",JSON.stringify(json))
+    }
+    else{
+     let json = JSON.parse(localStorage.getItem("todos"));
+     index++;
+     json.push({id:index,text:input.value});
+     localStorage.setItem("todos",JSON.stringify(json))
+    }
+    showTodos()
+    input.value = "";
+  }
+})
+
+
+
+
+
 
 
 
